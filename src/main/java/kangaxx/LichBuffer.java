6958941cf8bae1232079b8c230c27e457fb91385
Zhao Yao <zhaoyao.0xff@gmail.com>
2012-10-15 17:43:15 +0800

    Added number to string comment

--- src/main/java/kangaxx/LichBuffer.java
+++ src/main/java/kangaxx/LichBuffer.java
@@ -31,6 +31,7 @@ class LichBuffer {
         array[length++] = '>';
     }
 
+    //borrowed from jedis
     private final static int[] sizeTable = {9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999, Integer.MAX_VALUE};
 
     private final static byte[] DigitTens = {
